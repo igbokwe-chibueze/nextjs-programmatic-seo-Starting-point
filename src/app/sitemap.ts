fd0192b9dda5531542f9291f1f99197ea5057f42
igbokwe-chibueze <igbokwechibueze@gmail.com>
2025-03-06 12:02:59 +0100

    Created Sitemap file
    Signed-off-by: igbokwe-chibueze <igbokwechibueze@gmail.com>

---
+++ src/app/sitemap.ts
@@ -0,0 +1,32 @@
+import { getAllTags, locations } from "@/data/restaurants";
+import { MetadataRoute } from "next";
+
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const allTags = await getAllTags();
+
+  const searchLandingPages = allTags
+    .map((tag) =>
+      locations.map((location) => ({
+        url: `${baseUrl}/${location}/${tag}`,
+        lastModified: new Date(),
+        changeFrequency: "weekly",
+        priority: 1,
+      })),
+    )
+    .flat() as MetadataRoute.Sitemap;
+
+  return [
+    // Normally this is how you Insert your other pages:
+    // {
+    //   url: `${baseUrl}/about`,
+    //   lastModified: "2024-12-31",
+    //   changeFrequency: "yearly",
+    //   priority: 0.8,
+    // },
+    
+    // Our programmatic SEO pages:
+    ...searchLandingPages,
+  ];
+}
