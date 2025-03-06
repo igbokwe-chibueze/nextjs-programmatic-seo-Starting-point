bbc494ad6332b9b3ab8687c0f2cab1d255e9d19b
igbokwe-chibueze <igbokwechibueze@gmail.com>
2025-03-06 15:30:07 +0100

    added generateStaticParams to dynamic page
    Signed-off-by: igbokwe-chibueze <igbokwechibueze@gmail.com>

--- src/app/[location]/[q]/page.tsx
+++ src/app/[location]/[q]/page.tsx
@@ -1,12 +1,34 @@
 import Header from "@/components/Header";
 import RestaurantItem from "@/components/RestaurantItem";
-import { searchRestaurants } from "@/data/restaurants";
+import { getAllTags, locations, searchRestaurants } from "@/data/restaurants";
 import { Metadata } from "next";
 import { cache } from "react";
 
 interface PageProps {
     params: Promise<{ location: string; q: string}>
 }
+
+export const revalidate = 86400; // Refresh cached pages once every 24 hours
+
+export async function generateStaticParams() {
+    const allTags = await getAllTags({
+      // If you have very many pages, you can only render a subset at compile-time. 
+      // The rest will be rendered & cached at first access.
+      // So this would only return 10 tags, therefore 10 tags x 3 locations, would return only 30 pages.
+      // other pages outside this 30 would be cached and avialable for all users only when they are opened.
+      // limit: 10
+    });
+  
+    return allTags.map((tag) => locations.map((location) => ({
+        location,
+        q: tag,
+    }))).flat();
+}
+// If we do generateStaticParams this way, returning an empty area, 
+// it would catch a page (tag * location) the first time that dynamic page is rendered
+// export async function generateStaticParams() {
+//     return [];
+// }
 
 const getRestaurants = cache(searchRestaurants); //get all instances of the searched resturants
 
